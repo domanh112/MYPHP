c7753f3b8eb6330501dcc6ccd2f13167a6f91445
domanh112 <57127612+domanh112@users.noreply.github.com>
2019-11-21 20:19:49 -0800

    Add files via upload

---
+++ trangchu.php
@@ -0,0 +1,47 @@
+<HTML>
+
+<HEAD>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<link rel="stylesheet" type="text/css" href="../CSS/mystyle.css">
+
+<TITLE>Vietnam's number one watch</TITLE>
+
+
+</HEAD>
+
+<body>
+<img style="    width: 100%; height: 70%  ;" src="../images/img4.png" alt="">
+<img style="    width: 300px; height: 300px;" src="../images/gifdongho.gif" alt="">
+<img style="    width: 300px; height: 300px ;" src="../images/gifdongho2.gif" alt="">
+<img style="    width: 300px; height: 300px ;" src="../images/gifdongho3.gif" alt="">
+<img style="    width: 300px; height: 300px ;" src="../images/gifdongho4.gif" alt="">
+<img style="    width: 350px; height: 300px ;" src="../images/gifdongho5.gif" alt="">
+<img style="    width: 437px; height: 300px ;" src="../images/gifdongho6.gif" alt="">
+<nav id="fixNav">
+  <ul>
+    <li><a href="../test/login.php"><b>Login</b></a></li>
+    <li><a href="../test/registration.php"><b>Regestration</b></a></li>
+    <li><a href="../test/products.php"><b>Products</b></a>
+	<ul class="sub-menu">
+        <li><a href="../products/Men's Watch.php"><b>Men's Watch</b></a></li>
+        <li><a href="../products/Female Watch.php"><b>Female Watch</b></a></li>
+     </ul>
+    </li> 
+    <li><a href="../test/intro.php"><b>Introduction</b></a></li>
+    <li><a href="../test/trangchu.php"><b>Home</b></a></li>
+
+    <div class="container-1">
+      <span class="icon"><i class="fa fa-search"></i></span>
+      <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+      <input type="search" id="search" placeholder="Search..." />
+  </div>
+  </ul>
+</nav>
+
+
+
+</body>
+
+</HTML>
